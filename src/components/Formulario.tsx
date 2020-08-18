199981e4588292ea1bb54b889ce7a1d8fe281de9
mariousm <mariousm7@gmail.com>
2020-08-18 10:13:15 +0100

    Realizado los cálculos de la cotización

--- src/components/Formulario.tsx
+++ src/components/Formulario.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "@emotion/styled";
+import { obtenerDiferenciaYear, calcularMarca, obtenerPlan } from "../helper";
 
 // Definimos los estilos de este componente
 const Div = styled.div`
@@ -44,12 +45,74 @@ const Button = styled.button`
   }
 `;
 
+const Error = styled.div`
+  background-color: red;
+  color: white;
+  padding: 1rem;
+  width: 100%;
+  text-align: center;
+  margin-bottom: 2rem;
+`;
+
 const Formulario = () => {
+  // Definimos el state del componente
+  const [datos, guardarDatos] = useState({
+    marca: "",
+    year: "",
+    plan: "",
+  });
+  const [error, guardarError] = useState(false);
+
+  // Extramemos los valores
+  const { marca, year, plan } = datos;
+
+  // Obtener los datos del formulario y guardarlos en el state
+  const obtenerInformacion = (e: any) => {
+    guardarDatos({
+      ...datos,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  // Cuando el usuario presiona submit
+  const cotizarSeguro = (e: any) => {
+    e.preventDefault();
+
+    // Validamos
+    if (marca.trim() === "" || year.trim() === "" || plan.trim() === "") {
+      guardarError(true);
+      return;
+    }
+
+    guardarError(false);
+
+    // Tenemos una base de 2000€
+    let resultado: number = 2000;
+
+    // Obtener la diferencia de años
+    let nDiferenciaYears: number = obtenerDiferenciaYear(parseInt(year));
+
+    // Por cada año hay que restar el 3%
+    resultado -= (nDiferenciaYears * 3 * resultado) / 100;
+
+    // Dependiendo de la marca: Europero 30%, Americano 15%, Asiatico 5%
+    resultado = calcularMarca(marca) * resultado;
+
+    // Dependiendo del plan: Completo aumenta 50%, Básico aumenta 20%
+    let nIncremento: number = obtenerPlan(plan);
+    resultado = Math.round(nIncremento * resultado * 100) / 100;
+    console.log(resultado);
+
+    // Total
+  };
+
   return (
-    <form>
+    <form onSubmit={cotizarSeguro}>
+      {error === true ? <Error>Todos los campos son obligatorios</Error> : null}
+
       <Div>
         <Label>Marca</Label>
-        <Select>
+        <Select name="marca" value={marca} onChange={obtenerInformacion}>
           <option value="">-- Seleccione --</option>
           <option value="europeo">Europeo</option>
           <option value="americano">Americano</option>
@@ -58,7 +121,7 @@ const Formulario = () => {
       </Div>
       <Div>
         <Label>Año</Label>
-        <Select>
+        <Select name="year" value={year} onChange={obtenerInformacion}>
           <option value="">-- Seleccione --</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
@@ -74,10 +137,24 @@ const Formulario = () => {
       </Div>
       <Div>
         <Label>Plan</Label>
-        <InputRadio type="radio" name="plan" value="basico" /> Básico
-        <InputRadio type="radio" name="plan" value="completo" /> Completo
+        <InputRadio
+          type="radio"
+          name="plan"
+          value="basico"
+          checked={plan === "basico"}
+          onChange={obtenerInformacion}
+        />{" "}
+        Básico
+        <InputRadio
+          type="radio"
+          name="plan"
+          value="completo"
+          checked={plan === "completo"}
+          onChange={obtenerInformacion}
+        />{" "}
+        Completo
       </Div>
-      <Button type="button">Cotizar</Button>
+      <Button type="submit">Cotizar</Button>
     </form>
   );
 };
---
+++ src/helper.tsx
@@ -0,0 +1,30 @@
+// Obtiene la diferencia de años
+export function obtenerDiferenciaYear(year: number): number {
+  return new Date().getFullYear() - year;
+}
+
+// Calcula el total a pagar según la marca
+export function calcularMarca(marca: string): number {
+  let incremento: number = 0;
+
+  switch (marca) {
+    case "europeo":
+      incremento = 1.3;
+      break;
+    case "americano":
+      incremento = 1.15;
+      break;
+    case "asiatico":
+      incremento = 1.05;
+      break;
+    default:
+      break;
+  }
+
+  return incremento;
+}
+
+// Calcular el tipo de seguro
+export function obtenerPlan(plan: string): number {
+  return plan === "basico" ? 1.2 : 1.5;
+}
